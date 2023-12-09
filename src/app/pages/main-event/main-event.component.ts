053234072d02e1db67f3a6c060be4522e1a60904
alex <alexcolegrove93@gmail.com>
2023-12-08 21:58:26 -0500

    facelift

--- src/app/pages/main-event/main-event.component.ts
+++ src/app/pages/main-event/main-event.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
   selector: 'app-main-event',
   standalone: true,
-    imports: [CommonModule, MatIconModule],
+    imports: [CommonModule, MatIconModule, MatButtonModule],
   templateUrl: './main-event.component.html',
   styleUrl: './main-event.component.scss'
 })
