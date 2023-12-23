550cfcb5f914f31213cca4d617a17a851d9de161
alex <alexcolegrove93@gmail.com>
2023-12-22 23:56:48 -0500

    fallback

--- src/app/services/email.service.ts
+++ src/app/services/email.service.ts
@@ -1,33 +1,28 @@
 import { Injectable } from '@angular/core';
 import emailJs from "@emailjs/browser";
-import {Observable, of} from "rxjs";
 @Injectable({
     providedIn: 'root',
 })
 export class EmailService {
     private baseUrl = "https://mail"
-    private pubKey = process.env['MAIL_KEY'];
-    constructor() {
-        if (this.pubKey) {
-            emailJs.init(this.pubKey);
+    private pubKey = "dIL-mFJhcvYUGVYfs";
 
-        }
+    constructor() {
+        emailJs.init(this.pubKey);
     }
 
     async postMessage$(input: any): Promise<any> {
-        console.log(this.pubKey);
-        
-        // await emailJs.send(
-        //     "service_s3vhsur",
-        //     "template_4dlyj64",
-        //     {
-        //         email: input.email,
-        //         name: input.name,
-        //         willAttend: input.willAttend,
-        //         additionalGuests: input.additionalGuests,
-        //         dietaryRestrictions: input.dietaryRestrictions
-        //     },
-        // )
+        await emailJs.send(
+            "service_s3vhsur",
+            "template_4dlyj64",
+            {
+                email: input.email,
+                name: input.name,
+                willAttend: input.willAttend,
+                additionalGuests: input.additionalGuests,
+                dietaryRestrictions: input.dietaryRestrictions
+            },
+        )
     }
 
 }
